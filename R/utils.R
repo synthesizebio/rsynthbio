14ced96ab343c8ab1acfae53d26664f1d12bf4e5
Candace Savonen <cansav09@gmail.com>
2025-08-13 14:04:32 -0400

    Streamline release numbers

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,10 @@
 
+RELEASE_NUMBER <- "v2.2" 
+
 #' @title API Base URL
 #' @description Base URL for the Synthesize Bio API
 #' @export
-API_BASE_URL <- "https://app.synthesize.bio"
+API_BASE_URL <- paste0("https://app.synthesize.bio/api/model/", RELEASE_NUMBER)
 
 #' @title Model Modalities
 #' @description A nested list containing supported modalities for different
@@ -10,11 +12,16 @@ API_BASE_URL <- "https://app.synthesize.bio"
 #' + sra = this is bulk RNA-seq
 #' @format A nested list with structure: model type > version > modalities
 #' @export
-MODEL_MODALITIES <- list(
-  "v2.2" = list(
-    "bulk"
-  )
-)
+MODEL_MODALITIES <- list()
+MODEL_MODALITIES[[RELEASE_NUMBER]] <- list("bulk")
 
-utils::globalVariables(c("sample_group", "sample_index"))
+# Alternative approach using setNames():
+# MODEL_MODALITIES <- setNames(list(list("bulk")), RELEASE_NUMBER)
+
+# Now you can access it with:
+# MODEL_MODALITIES$"v2.2"
+# or
+# MODEL_MODALITIES[["v2.2"]]
+# or 
+# MODEL_MODALITIES[[RELEASE_NUMBER]]
 
