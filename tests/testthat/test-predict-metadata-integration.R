f92f79c7367e7d4b975a82349a7e0c70485ef39f
jefk <jefk@users.noreply.github.com>
2025-12-02 16:45:13 -0800

    add tests for new models

---
+++ tests/testthat/test-predict-metadata-integration.R
@@ -0,0 +1,33 @@
+library(testthat)
+library(jsonlite)
+library(httr)
+
+# Helper function to check if API key is available
+api_key_available <- function() {
+    !is.na(Sys.getenv("SYNTHESIZE_API_KEY")) &&
+        Sys.getenv("SYNTHESIZE_API_KEY") != ""
+}
+
+run_slow_tests <- function() {
+    # this endpoint is scaled to zero, so only run this test when the env var is set
+    tolower(Sys.getenv("RUN_SLOW_TESTS")) == "true"
+}
+
+test_that("predict metadata endpoint returns valid data", {
+    skip_if_not(
+        run_slow_tests(),
+        "Skipping slow test because RUN_SLOW_TESTS is not set."
+    )
+    skip_if_not(
+        api_key_available(),
+        "Skipping live API test because SYNTHESIZE_API_KEY is not set."
+    )
+
+    query <- get_example_query(model_id = "gem-1-bulk_predict-metadata")$example_query
+    results <- predict_query(query = query, model_id = "gem-1-bulk_predict-metadata")
+    print(names(results))
+    expect_type(results, "list")
+    expect_true("classifier_probs" %in% names(results))
+    expect_s3_class(results$metadata, "data.frame")
+    expect_true(nrow(results$metadata) > 0)
+})
